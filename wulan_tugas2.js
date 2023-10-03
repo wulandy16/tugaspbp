71c46082ff94263e8ec22fe9b8084bc1fa24d507
wulandy16 <146553577+wulandy16@users.noreply.github.com>
2023-10-03 22:20:11 +0700

    Add files via upload

---
+++ wulan_tugas2.js
@@ -0,0 +1,9 @@
+const firstName = "Wulan";
+const lastName = "Dwi Yanti";
+const age = 21; 
+const isMarried = false; 
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Age:", age);
+console.log("Is Married:", isMarried);
