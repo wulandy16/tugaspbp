7383bbb30603835177c72b1db58a6d661d6c7487
wulandy16 <146553577+wulandy16@users.noreply.github.com>
2023-10-17 20:29:50 +0700

    Add files via upload

---
+++ tugas3no1.js
@@ -0,0 +1,9 @@
+const restaurant = {
+    name: "Mc Donald",
+    city: "Tangerang Selatan",
+    "favorite drink": "Ice Lemon Tea",
+    "favorite food": "Double Big Mac",
+    isVegan: false,
+};
+
+console.table(restaurant)
